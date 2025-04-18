497ea6f07d7316dca35fdd37803215509680ddb3
amira qasim <amirakassem855@gmail.com>
2025-04-18 13:52:25 +0200

    Add admin

--- DB/connection.js
+++ DB/connection.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
 export const connectDb = async () => {
+  console.log(process.env.CONNECTION_URL);
+  
   return await mongoose
     .connect(process.env.CONNECTION_URL)
     .then(() => console.log("DB connected successfully"))
